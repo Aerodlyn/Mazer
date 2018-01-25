e5fc78762f2917f764747ccf705ca600a5aa3967
Patrick Jahnig <psj516@vt.edu>
2018-01-25 00:28:57 -0500

    Comments and removal of one thrown error

--- Headers/Vector.h
+++ Headers/Vector.h
@@ -23,7 +23,7 @@ void Vector_append (Vector * const vector, const void * const element);
 
 /**
  * Frees dynamically created objects (allocated using malloc, calloc, etc.) that are stored within 
- *  this Vector instance.
+ *  the given Vector instance.
  * 
  * @param vector    The Vector instance to free
  * @param f         The function to use to free each object stored within this Vector instance
@@ -34,20 +34,59 @@ void Vector_free_d (Vector *vector, Vector_freeFunction f);
 
 /**
  * Frees statically created objects (not allocated using malloc, calloc, etc.) that are stored 
- *  within this Vector instance.
+ *  within the given Vector instance.
  * 
  * @param vector The Vector instance to free
  */
 void Vector_free_s (Vector *vector);
 
+/**
+ * Returns the element at the given index of the given Vector instance.
+ * 
+ * @param vector    The Vector instance that contains the element to return
+ * @param index     The index of the element to return
+ * 
+ * @return The element at the given index of the given Vector instace
+ * 
+ * NOTE: Throws an error and terminates the program if the given index is out of the Vector
+ *          instance's range of elements.
+ */
 void* Vector_get (const Vector * const vector, const unsigned int index);
 
+/**
+ * Resizes the given Vector instance such that its capacity is equal to the given size.
+ * 
+ * @param vector    The Vector instance to resize
+ * @param size      The new capacity of the Vector instance
+ * 
+ * NOTE: Throws an error and terminates the program if an attempt to resize the Vector instace
+ *          below its number of elements is made.
+ */
 void Vector_resize (Vector * const vector, const unsigned int size);
 
+/**
+ * Removes the last element in the given Vector instance and frees it using the given function. Does
+ *  nothing if the given Vector instance is empty.
+ * 
+ * @param vector    The Vector instance to remove the last element from
+ * @param f         The function to use to free the last object stored within the given Vector 
+ *                      instance; NOTE: Can be null, however no free call will be made on the 
+ *                      last object in the Vector instance.
+ */
 void Vector_truncate_d (Vector * const vector, Vector_freeFunction f);
 
+/**
+ * Removes the last element in the given Vector instance.
+ * 
+ * @param vector The Vector instance to remove the last element from
+ */
 void Vector_truncate_s (Vector * const vector);
 
-Vector *Vector_new ();
+/**
+ * Allocates and returns a new Vector instance with an initial capacity of 10.
+ * 
+ * @return A new and empty Vector instance
+ */
+Vector* Vector_new ();
 
 #endif 
--- Sources/Vector.c
+++ Sources/Vector.c
@@ -14,7 +14,7 @@ void Vector_append (Vector * const vector, const void * const element)
 
 /**
  * Frees dynamically created objects (allocated using malloc, calloc, etc.) that are stored within 
- *  this Vector instance.
+ *  the given Vector instance.
  * 
  * @param vector    The Vector instance to free
  * @param f         The function to use to free each object stored within this Vector instance
@@ -37,12 +37,23 @@ void Vector_free_d (Vector *vector, Vector_freeFunction f)
 
 /**
  * Frees statically created objects (not allocated using malloc, calloc, etc.) that are stored 
- *  within this Vector instance.
+ *  within the given Vector instance.
  * 
  * @param vector The Vector instance to free
  */
 void Vector_free_s (Vector *vector) { Vector_free_d (vector, NULL); }
 
+/**
+ * Returns the element at the given index of the given Vector instance.
+ * 
+ * @param vector    The Vector instance that contains the element to return
+ * @param index     The index of the element to return
+ * 
+ * @return The element at the given index of the given Vector instace
+ * 
+ * NOTE: Throws an error and terminates the program if the given index is out of the Vector
+ *          instance's range of elements.
+ */
 void* Vector_get (const Vector * const vector, const unsigned int index)
 {
     if (index >= vector->size)
@@ -51,6 +62,15 @@ void* Vector_get (const Vector * const vector, const unsigned int index)
     return vector->data [index];
 }
 
+/**
+ * Resizes the given Vector instance such that its capacity is equal to the given size.
+ * 
+ * @param vector    The Vector instance to resize
+ * @param size      The new capacity of the Vector instance
+ * 
+ * NOTE: Throws an error and terminates the program if an attempt to resize the Vector instace
+ *          below its number of elements is made.
+ */
 void Vector_resize (Vector * const vector, const unsigned int size)
 {
     if (size < vector->size)
@@ -60,10 +80,19 @@ void Vector_resize (Vector * const vector, const unsigned int size)
     vector->data        = realloc (vector->data, vector->capacity);
 }
 
+/**
+ * Removes the last element in the given Vector instance and frees it using the given function. Does
+ *  nothing if the given Vector instance is empty.
+ * 
+ * @param vector    The Vector instance to remove the last element from
+ * @param f         The function to use to free the last object stored within the given Vector 
+ *                      instance; NOTE: Can be null, however no free call will be made on the 
+ *                      last object in the Vector instance.
+ */
 void Vector_truncate_d (Vector * const vector, Vector_freeFunction f)
 {
     if (Vector_isEmpty (vector))
-        throwError ("Cannot truncate from an empty Vector instance.", -1);
+        return;
 
     if (f)
         f (vector->data [--vector->size]);
@@ -72,9 +101,19 @@ void Vector_truncate_d (Vector * const vector, Vector_freeFunction f)
         vector->size--;
 }
 
+/**
+ * Removes the last element in the given Vector instance.
+ * 
+ * @param vector The Vector instance to remove the last element from
+ */
 void Vector_truncate_s (Vector * const vector) { Vector_truncate_d (vector, NULL); }
 
-Vector *Vector_new ()
+/**
+ * Allocates and returns a new Vector instance with an initial capacity of 10.
+ * 
+ * @return A new and empty Vector instance
+ */
+Vector* Vector_new ()
 {
     Vector *vector = calloc (1, sizeof (struct _Vector));
 
