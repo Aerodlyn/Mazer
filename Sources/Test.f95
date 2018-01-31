a6802f68b6b185f9bb385d375792655f454379a8
Patrick Jahnig <psj516@vt.edu>
2018-01-31 17:02:51 -0500

    I believe that the algorithm now does what it is supposed to.
    I will test it by calling it from the C codebase soon.

--- Sources/Test.f95
+++ Sources/Test.f95
@@ -1,27 +1,82 @@
 program Test
     implicit none
 
-    integer, parameter :: ATTEMPTS = 5
     integer, parameter :: ROOM_POINTS = 4
+    integer, allocatable :: rooms (:, :)
+    integer :: i
 
-    integer :: roomsGenerated
-    integer :: rooms (5, 4) = 0
- 
-    roomsGenerated = generateRooms (rooms)
-    
+    rooms = generateRooms (50, 10, 100, 1)
+    print *, ""
+    do i = 1, size (rooms) / 4
+        print "(4I4)", rooms (i, :)
+    end do
+
 contains
-    integer function generateRooms (rooms)
+    subroutine testFuc ()
+        print *, "Hello"
+    end subroutine testFuc
+    
+    function generateRooms (attempts, minSize, screenSize, padding) result (rooms)
         implicit none
 
-        integer, intent (inout) :: rooms (:, :)
+        integer, intent (in) :: attempts, minSize, screenSize, padding
+        integer :: sAttempts = 0
+
+        integer, allocatable :: rooms (:, :)
+        integer :: rooms_s (attempts, 4)
 
         integer :: i, j
-        do i = 1, ATTEMPTS
-            do j = 1, ROOM_POINTS
-                rooms (i, j) = mod (generateRandomNumber (), 900)
+        logical :: add = .true.
+        do i = 1, attempts
+            do j = 1, 4
+                rooms_s (i, 1) = mod (generateRandomNumber (), screenSize)
+                rooms_s (i, 2) = mod (generateRandomNumber (), screenSize)
+                rooms_s (i, 3) = mod (generateRandomNumber (), minSize) + minSize
+                rooms_s (i, 4) = mod (generateRandomNumber (), minSize) + minSize
+
+                if (rooms_s (i, 1) < padding) rooms_s (i, 1) = padding
+
+                if (rooms_s (i, 2) < padding) rooms_s (i, 2) = padding
+
+                if (rooms_s (i, 1) + rooms_s (i, 3) > screenSize - padding) rooms_s (i, 1) = screenSize - rooms_s (i, 3)
+
+                if (rooms_s (i, 2) + rooms_s (i, 4) > screenSize - padding) rooms_s (i, 2) = screenSize - rooms_s (i, 4)
             end do
-            
-            print *, rooms (i, :)
+
+            print *, rooms_s (i, :)
+            do j = 1, i - 1
+                if (rooms_s (j, 1) == -1) cycle
+
+                if (rooms_s (i, 1) + rooms_s (i, 3) < rooms_s (j, 1) &
+                    .or. rooms_s (j, 1) + rooms_s (j, 3) < rooms_s (i, 1)) then
+                    add = .true.
+
+                else if (rooms_s (i, 2) + rooms_s (i, 4) < rooms_s (j, 2) &
+                    .or. rooms_s (j, 2) + rooms_s (j, 4) < rooms_s (i, 2)) then
+                    add = .true.
+
+                else
+                    add = .false.
+                    exit
+                end if
+            end do
+
+            if (add) then
+                sAttempts = sAttempts + 1
+
+            else
+                rooms_s (i, 1) = -1
+            end if
+        end do
+
+        allocate (rooms (sAttempts, 4))
+
+        j = 1
+        do i = 1, attempts
+            if (rooms_s (i, 1) /= -1) then
+                rooms (j, :) = rooms_s (i, :)
+                j = j + 1
+            end if
         end do
     end function generateRooms
 
