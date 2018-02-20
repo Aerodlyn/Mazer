a9a3ce5185f8b497d3411b85159d34425843250a
Patrick Jahnig <psj516@vt.edu>
2018-02-19 20:00:37 -0500

    Fixed ALLEGRO_COLOR issue

--- Sources/Mazer_Module.f95
+++ Sources/Mazer_Module.f95
@@ -20,58 +20,23 @@ module Mazer
     end type
     
     interface
-        integer (c_int) function rand () bind (C, name = "rand")
+        integer (c_int) function c_rand () bind (C, name = "rand")
             use, intrinsic :: iso_c_binding
             implicit none
-        end function rand
-
-        type (ALLEGRO_COLOR) function al_map_rgb (r, g, b) bind (C, name = "al_map_rgb")
-            use, intrinsic :: iso_c_binding
-            import
-            implicit none
-        
-            integer (c_signed_char) :: r, g, b
-        end function al_map_rgb
-
-        type (ALLEGRO_COLOR) function al_map_rgb_f (r, g, b) bind (C, name = "al_map_rgb_f")
-            use, intrinsic :: iso_c_binding
-            import
-            implicit none
-        
-            real (kind = 4) :: r, g, b
-        end function al_map_rgb_f
-
-        logical function f_validStarter (tiles, x, y, perSide, padding)
-            use, intrinsic :: iso_c_binding
-            use Mazer
-            implicit none
-            
-            integer (c_int16_t), intent (in) :: x, y, perSide
-            integer (c_int8_t), intent (in) :: padding
-            type (Tile), dimension (perSide * perSide), intent (in) :: tiles
-            
-            f_validStarter = .false.
-            if (.not. tiles ((x + perSide * y) + 1_c_int16_t)%valid) then
-                f_validStarter = .false.
-            
-            else
-                if (x > padding + 1_c_int8_t) then
-                    if (.not. tiles (((x - 1_c_int16_t) + perSide * y) + 1_c_int16_t)%valid) f_validStarter = .true.
-                end if
-            
-                if (x < perSide - padding - 1_c_int8_t) then
-                    if (.not. tiles (((x + 1_c_int16_t) + perSide * y) + 1_c_int16_t)%valid) f_validStarter = .true.
-                end if
-            
-                if (y > padding + 1_c_int8_t) then
-                    if (.not. tiles ((x + perSide * (y - 1_c_int16_t)) + 1_c_int16_t)%valid) f_validStarter = .true.
-                end if
-            
-                if (y < perSide - padding - 1_c_int8_t) then
-                    if (.not. tiles ((x + perSide * (x + 1_c_int16_t)) + 1_c_int16_t)%valid) f_validStarter = .true.
-                end if
-            end if 
-        end function f_validStarter
+        end function c_rand
     end interface
+    
+    contains
+        type (ALLEGRO_COLOR) function al_map_rgb_f (r, g, b)
+            use, intrinsic :: iso_c_binding
+            implicit none
+        
+            real (c_float), intent (in) :: r, g, b
+
+            al_map_rgb_f%r = r 
+            al_map_rgb_f%g = g 
+            al_map_rgb_f%b = b 
+            al_map_rgb_f%a = 1.0_c_float
+        end function al_map_rgb_f
 end module Mazer
 
